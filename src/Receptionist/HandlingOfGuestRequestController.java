e6952409be14e54e5d3f653032be6830d9f8e7a5
Masud <Masud@LAPTOP-7361INU5>
2024-04-16 01:28:58 +0600

    Eco_Resort_System

---
+++ src/Receptionist/HandlingOfGuestRequestController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Receptionist;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Masud
+ */
+public class HandlingOfGuestRequestController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
